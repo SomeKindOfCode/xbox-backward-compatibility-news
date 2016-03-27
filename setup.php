481ddb13e4c1f1356349c61d2dc29de953e6d834
Christopher Beloch <c.beloch@opwoco.de>
2016-03-28 00:14:59 +0200

    add autoincrement to setup

--- setup.php
+++ setup.php
@@ -5,8 +5,11 @@ require 'app/bootstrap.php';
 
 // SQLite3
 $database->query('CREATE TABLE IF NOT EXISTS games (
-	id     INT     PRIMARY KEY,
+	id     INTEGER     PRIMARY KEY 	AUTOINCREMENT,
 	name   TEXT,
     slug   TEXT,
     image  TEXT
 )');
+
+
+var_dump($database->error());
