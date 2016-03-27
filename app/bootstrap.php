fcee46b7e2a4fe6ff409c59685cd2cc7e2636b98
Christopher Beloch <c.beloch@opwoco.de>
2016-03-27 23:01:31 +0200

    added app bootstrap with database setup

---
+++ app/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+$database = new medoo([
+    'database_type' => 'sqlite',
+    'database_file' => __DIR__ . '/../db/db.sqlite'
+]);
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../app/bootstrap.php';
 
 Flight::route('/', function(){
     echo 'hello world!';
