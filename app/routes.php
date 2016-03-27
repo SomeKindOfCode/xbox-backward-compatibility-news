d4c07846f35161bd8443ecb5290315e30a0c1fa7
Christopher Beloch <c.beloch@opwoco.de>
2016-03-28 00:46:19 +0200

    comments

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+// Flight supports arrays as controller and their static functions
+
 Flight::route('/', ['XboxController', 'index']);
 Flight::route('/feed', ['XboxController', 'feed']);
 Flight::route('/feed/weekly', ['XboxController', 'feedWeekly']);
