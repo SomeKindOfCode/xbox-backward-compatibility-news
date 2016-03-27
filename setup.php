170ddcd013ea5075cb9facae2afea7c9fd31f099
Christopher Beloch <c.beloch@opwoco.de>
2016-03-28 01:38:07 +0200

    added import date

--- setup.php
+++ setup.php
@@ -5,11 +5,11 @@ require 'app/bootstrap.php';
 
 // SQLite3
 $database->query('CREATE TABLE IF NOT EXISTS games (
-	id     INTEGER     PRIMARY KEY 	AUTOINCREMENT,
-	name   TEXT,
-    slug   TEXT,
-    image  TEXT
+	id     			INTEGER     PRIMARY KEY 	AUTOINCREMENT,
+	name   			TEXT,
+    slug   			TEXT,
+    image  			TEXT,
+	date_imported 	DATETIME
 )');
-
 
 var_dump($database->error());
