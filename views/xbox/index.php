9dfa249a680c8334275204afa8ed4448acf3d5a1
Christopher Beloch <c.beloch@opwoco.de>
2016-03-28 16:09:52 +0200

    added website rendering

--- views/xbox/index.php
+++ views/xbox/index.php
@@ -2,12 +2,41 @@
 <html>
     <head>
         <title>New Xbox Backward Compatibility Games</title>
+        <style>
+            html, body {
+                margin: 0px;
+                font-family: -apple-system, Helvetica, Arial, sans-serif;
+                background: #333;
+                color: white;
+            }
+            h1, h2, h3 {
+                font-weight: lighter;
+            }
+            .container {
+                margin: 15px auto;
+                max-width: 920px;
+            }
+        </style>
     </head>
     <body>
-        <?php foreach($gamesByWeek as $weeklyGames){ ?>
+        <div class="container">
+            <h1>Weekly Backwards Compatibility</h1>
+        <?php foreach($gamesByWeek as $week => $weeklyGames){ ?>
+            <h2>
+                <?php
+                $input = explode('-', $week);
+                list($year, $weekNo) = $input;
+
+                $weekDate = new DateTime();
+                $weekDate->setISODate($year, $weekNo);
+
+                echo $weekDate->format('\W\e\e\k W - Y');
+                ?>
+            </h2>
             <?php foreach($weeklyGames as $singleGame){ ?>
                 <img src="<?= $singleGame["image"] ?>"\>
             <?php } ?>
         <?php } ?>
+        </div>
     </body>
 </html>
