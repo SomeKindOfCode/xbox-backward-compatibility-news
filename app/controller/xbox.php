ab38859d0b140a9055410e01bb93d0758f1ca768
Christopher Beloch <c.beloch@opwoco.de>
2016-03-29 08:17:22 +0200

    fix dayly qwery

--- app/controller/xbox.php
+++ app/controller/xbox.php
@@ -162,7 +162,10 @@ class XboxController {
         $groupedGames = [];
 
         $games = self::getGames([
-            'date_imported[<]' => (new DateTime())->format('Y-m-d 00:00:00') // Don't include the current day
+            'date_imported[<]' => sprintf(
+                'Datetime(%s)',
+                (new DateTime())->format('Y-m-d 00:00:00') // Don't include the current day
+            )
         ]);
 
         foreach($games as $singleGame) {
