550307a001cfc2f4f829df0fbd10a79f7f03f268
Christopher Beloch <c.beloch@opwoco.de>
2016-04-03 01:33:06 +0200

    bootstrap smarty

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,26 @@ $database = new medoo([
     'database_file' => __DIR__ . '/../db/db.sqlite'
 ]);
 
-Flight::set('flight.views.path', __DIR__ . '/../views');
+// Register Smarty as the view class
+Flight::register('view', 'Smarty', array(), function($smarty){
+    $smarty->template_dir = __DIR__ . '/views/';
+    $smarty->compile_dir = __DIR__ . '/views_c/';
+    $smarty->config_dir = __DIR__ . '/config/';
+    $smarty->cache_dir = __DIR__ . '/../cache/';
+
+    $smarty->registerPlugin("modifier","DateTime", "dateTimeToDate");
+});
+Flight::map('render', function($template, $data){
+    Flight::view()->assign($data);
+    Flight::view()->display($template);
+});
+
+
+
+function dateTimeToDate($input)
+{
+    return DateTime::createFromFormat('Y-m-d H:i:s', $input)->format('Y-m-d');
+}
 
 // Setup Controller
 require 'controller/cache.php';
